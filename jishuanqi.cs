f39d61ea421358c8d1c061824b5da2b18ff1a2c3
川~~ <1390322466@qq.com>
2020-05-28 18:42:30 +0800

    Add files via upload

---
+++ jishuanqi.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_8
+{
+    class jishuanqi
+    {
+        public string a;
+        public string b;
+        public string c;
+        public int e;
+        public int f;
+        public void setmessage()
+        {
+            a = Console.ReadLine();
+            c = Console.ReadLine();
+            b = Console.ReadLine();
+        }
+        public void getmessage()
+        {
+            int d = 0;
+            if (int.TryParse(a, out d) && int.TryParse(b, out d))
+            {
+                e = int.Parse(a);
+                f = int.Parse(b);
+                jisuan();
+            }
+            else
+                Connection();
+
+        }
+        public void Connection()
+        {
+            string d;
+            if (c == "+")
+            {
+                d = a + b;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+            else
+                if (c == "-")
+            {
+                d = a.Replace(b, "");
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+            else
+                Console.WriteLine("输入错误");
+        }
+        public void jisuan()
+        {
+            float d;
+            if (c == "+")
+            {
+                d = e + f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+
+            else
+                if (c == "-")
+            {
+                d = e - f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+
+            else
+                if (c == "*")
+            {
+                d = e * f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+
+            else
+                if (c == "/")
+            {
+                d = e / f;
+                Console.WriteLine(a + c + b + '=' + d);
+            }
+            else
+                Console.WriteLine("符号输入错误");
+        }
+    }
+}
